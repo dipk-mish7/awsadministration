7920b1b865d634b41f16d43568ec0899bb3edd9e
deepak <deepak@example.com>
2021-08-08 04:08:48 +0530

    created users profile

--- IAM/createusers.sh
+++ IAM/createusers.sh
@@ -3,4 +3,5 @@
 for name in BlackPanther CaptainAmerica Thor Hulk BlackWidow Groot Nebula Spiderman
 do
  aws iam create-user --user-name $name
+ aws iam create-login-profile --user-name $name --password India@123 --password-reset-required
 done
